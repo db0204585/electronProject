2bb362ab85e96cab035c2993645a113ad7ac4405
David Blacksher <db0204585@otc.edu>
2017-12-11 09:14:36 -0600

    Third Commit more working at school

--- main.js
+++ main.js
@@ -1,60 +1,71 @@
-const electron = require("electron");
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-const Menu = electron.Menu;
-const ipc = electron.ipcMain;
-//const remote = require("electron").remote;
-//const electronFs = remote.require("fs");
-//const electronDialog = remote.dialog;
-
-app.on('ready', _=>{
-    console.log("sup w0rld")
-    mainWindow = new BrowserWindow({ width:300, height: 500})
-    mainWindow.loadURL(`file://${__dirname}/index.html`)
-
-    const menu = Menu.buildFromTemplate(customMenu);
-    Menu.setApplicationMenu(menu)
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+const Menu = electron.Menu
+const {dialog} = require('electron')
 
 
-    mainWindow.on('closed', _=>{
+
+app.on('ready', function(){
+    //console.log("electron running!")
+    mainWindow = new BrowserWindow({
+ })
+
+ mainWindow.loadURL(`file://${__dirname}/index.html`)
+
+ const menu = Menu.buildFromTemplate(template)
+ Menu.setApplicationMenu(menu)
+
+    mainWindow.on('closed', function(){
+        console.log('closed')
         mainWindow = null
-        console.log('close')
     })
 })
 
-const customMenu = [
-    {
-    label: "File",
-    submenu : [
-            {label : "Open File"
-            },
-            {type: "separator"
-            }, 
-            {label : "Quit",
-            click: _=>{
-                app.quit()
-            },
-             accelerator: "Ctrl+Q"}
-                                
-            ]
-        },{
-            label : "Dev Tools",
-            click : function(item, focusedWindow){
-                focusedWindow.toggleDevTools();
-            },
-            accelerator: "Ctrl+T"
-        }
-    ]
+var showOpen = function(){
+    dialog.showOpenDialog({properties: ['openFile'],})
+}
 
-ipc.on('countdown-start', (evt,arg) =>{
-    let count = 3
-    let timer = setInterval(_=>{
-        console.log("count" + count)
-        count--
-    mainWindow.webContents.send('countdown', count)
-    //
-    if (count == 0){
-        clearInterval(timer)
+const template = [
+    {
+        label: "File",
+        submenu: [
+            {
+                label: 'Open',
+                click: function(){
+                   showOpen()
+                }
+            },
+        ]
+       },{
+            label:"Edit",
+            submenu:[
+                {
+                    label:"Add Item"
+                },
+                {
+                    type: "separator"
+                },
+                {
+                    label:"Clear Items"
+                },
+                {
+                    type: "separator"
+                },
+                {
+                    label: "Quit",
+                    click: function(){
+                        app.quit()
+                    },
+                    accelerator: 'Ctrl+Q'
+                }
+                
+            ]      
+    },{
+        label:"Dev Tools",
+        click: function(item, focusedWindow){
+            focusedWindow.toggleDevTools()
+        },
+        accelerator: 'ctrl+I'
     }
-    }, 1000)
-})
+]
